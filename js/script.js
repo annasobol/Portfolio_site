ca23bd8132f39366903145d3c0fc209cdf94838a
Anna_Sobol <sobolanua@gmail.com>
2019-05-20 08:52:20 +0300

    hw_20.05

--- js/script.js
+++ js/script.js
@@ -3,4 +3,22 @@ $(document).ready(function () {
         $(this).toggleClass('active');
         $('.navigation-wrapper').fadeToggle(200);
     })
+
+ //    $('.navigation-wrapper a').click(function(){
+ //    $('.menu-button').animate({
+ //        scrollTop: $( $.attr(this, 'href') ).offset().top
+ //    }, 500);
+ //    return false;
+	// });
+
+    $('.navigation-wrapper a').click(function(){
+        $('.menu-button').removeClass('active');
+        $('.navigation-wrapper').fadeOut(200);
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return false;
+    });
+
+
 });
